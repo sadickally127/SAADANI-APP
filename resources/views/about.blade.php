7bc9e34b75f401a8920bf0a3a7e91fa5274da3ab
SADICK ALLY <sadickally127@gmail.com>
2024-08-05 09:21:58 +0300

    saadani app's files

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+<h1>This is the about page of the website, containing information parteining to the Website and the Website owners.</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+<h1>This is the contact page containing the contact information for the website owners.</h1> 
+</x-layout>
